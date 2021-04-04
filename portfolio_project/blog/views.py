3520022f229233f14686a421660f29bd4c53afdb
CA\xucao <xucao@deloitte.ca>
2021-04-03 20:49:47 -0400

    init dj11

---
+++ portfolio_project/blog/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Blog
+# Create your views here.
+def allblogs(request):
+    blogs=Blog.objects.all
+    return render(request,"blog/allblog.html",{"blogs":blogs})
